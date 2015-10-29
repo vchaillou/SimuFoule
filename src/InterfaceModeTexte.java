d9280380b3f374c534caf4a0c58a198a38665f66
vchaillou <valentin.chaillou@laposte.net>
2015-10-29 22:04:00 +0100

    Ajout Interface (premier jet)

---
+++ src/InterfaceModeTexte.java
@@ -0,0 +1,24 @@
+
+public class InterfaceModeTexte implements Interface {
+
+	private Simulateur simulateur;
+	
+	public InterfaceModeTexte(Simulateur unSimulateur) {
+		simulateur = unSimulateur;
+	}
+	
+	@Override
+	public void afficherMap() {
+		while(true) {
+			simulateur.lancerTour();
+			Graphe uneMap = simulateur.getMap();
+			for(int i=0 ; i<uneMap.getNbColonnes() ; i++) {
+				for(int j=0 ; j<uneMap.getNbLignes() ; j++) {
+					System.out.print(uneMap.getNode(i, j));
+				}
+				System.out.println("");
+			}
+			Thread.sleep(1000);
+		}
+	}
+}
---
+++ src/Lanceur.java
@@ -0,0 +1,9 @@
+
+public class Lanceur {
+
+	public static void main(String[] args) {
+		Interface uneInterface = new InterfaceModeTexte(new Simulateur());
+		uneInterface.afficherMap();
+	}
+
+}
