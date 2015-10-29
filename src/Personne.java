b50722346f12c97160165367232648ff954036e0
salfai <salfai.fc@gmail.com>
2015-10-29 22:21:07 +0100

    -Ajout du constructeur Personne

--- src/Personne.java
+++ src/Personne.java
@@ -5,21 +5,25 @@ import java.util.List;
 public class Personne {
 
 	public Case cCase;
-	
-	public void setCase(Case uneCase){
+
+	public Personne(Case cCase) {
+		super();
+		this.cCase = cCase;
+	}
+
+	public void setCase(Case uneCase) {
 		cCase.setOccupee(false);
 		this.cCase = uneCase;
 		cCase.setOccupee(true);
 	}
-	
-	
-	public void faireTour(List<ParcoursObservateur> observateurs){
+
+	public void faireTour(List<ParcoursObservateur> observateurs) {
 		for (ParcoursObservateur obs : observateurs) {
 			obs.faireTour(this, cCase);
 		}
 	}
-	
-	public Case getCase(){
+
+	public Case getCase() {
 		return this.cCase;
 	}
 }
